d9efc80494c94b8fefd0dcfff8663fb746763e61
Wenhao Zhao <zhaowh1987@gmail.com>
2026-08-05 09:32:44 +0800

    add local validation scripts

---
+++ scripts/check-js.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { execFileSync } = require('node:child_process')
+const { readdirSync } = require('node:fs')
+const { join } = require('node:path')
+
+function collectJavaScriptFiles(directory) {
+  return readdirSync(directory, { withFileTypes: true }).flatMap((entry) => {
+    const path = join(directory, entry.name)
+
+    if (entry.isDirectory()) {
+      return collectJavaScriptFiles(path)
+    }
+
+    return entry.isFile() && entry.name.endsWith('.js') ? [path] : []
+  })
+}
+
+const files = [
+  'game.js',
+  ...collectJavaScriptFiles('js'),
+  ...collectJavaScriptFiles('tests')
+].sort()
+
+for (const file of files) {
+  execFileSync(process.execPath, ['--check', file], { stdio: 'inherit' })
+}
+
+console.log(`Checked ${files.length} JavaScript files.`)
